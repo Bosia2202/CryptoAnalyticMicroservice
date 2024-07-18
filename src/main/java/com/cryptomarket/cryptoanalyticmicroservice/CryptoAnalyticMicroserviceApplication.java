d1656cd6b10116045911c77f15de368f0e75959f
Denis Vasilenko <denisvasilenko9@gmail.com>
2024-07-18 20:19:02 +0300

    first commit

---
+++ src/main/java/com/cryptomarket/cryptoanalyticmicroservice/CryptoAnalyticMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cryptomarket.cryptoanalyticmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoAnalyticMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoAnalyticMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptomarket/cryptoanalyticmicroservice/CryptoAnalyticMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptomarket.cryptoanalyticmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoAnalyticMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
